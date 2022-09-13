d83b86d2dcbc6b37be9335c5e3309376eb8391a5
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-09-13 14:04:53 +0200

    implemented test for resolution method

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -11,4 +11,14 @@ public class RegExprTests
 
     result.Should().BeEquivalentTo(new List<string> { "This", "is", "the", "best", "string", "you", "will", "see", "tselkjt", "sdf" });
   }
+
+  [Fact]
+  public void reusolutions_given__returns_()
+  {
+    var input = "1920x1080, 1024x780-987x650";
+
+    var result = RegExpr.Resolution(input);
+
+    result.Should().BeEquivalentTo(new List<(int, int)> { (1920, 1080), (1024, 780), (987, 650) });
+  }
 }
