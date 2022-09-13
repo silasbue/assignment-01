fe50aa28d60a70dd8abfad44865372de97cb4268
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-09-13 13:56:46 +0200

    updated test for splitline method

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -3,12 +3,12 @@ namespace Assignment1.Tests;
 public class RegExprTests
 {
   [Fact]
-  public void TestName()
+  public void splitLine_given_list_of__string_returns_words()
   {
-    var list = new List<string> { "This-is.the best/string.you will  see" };
+    var list = new List<string> { "This-is.the best/string.you will  see", "tselkjt.sdf" };
 
     var result = RegExpr.SplitLine(list);
 
-    result.Should().BeEquivalentTo(new List<string> { "This", "is", "the", "best", "string", "you", "will", "see" });
+    result.Should().BeEquivalentTo(new List<string> { "This", "is", "the", "best", "string", "you", "will", "see", "tselkjt", "sdf" });
   }
 }
