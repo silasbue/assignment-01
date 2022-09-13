995e780bf14de8ae256225698e384372be97d4b4
Silas Bue Handgaard <silasbueh@hotmail.com>
2022-09-13 06:57:21 +0200

    implemented flatten method

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,7 +2,16 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+  public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+  {
+    var output = new List<T>();
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    foreach (var item in items)
+      output.AddRange(item);
+
+    return output;
+  }
+
+  public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+    => throw new NotImplementedException();
 }
