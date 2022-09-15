342b6f343afaee0a952c9fa62d644734d3823e19
Silas Bue Handgaard <48789220+silasbue@users.noreply.github.com>
2022-09-15 20:32:19 +0200

    Implemented InnerText method (#6)

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -3,37 +3,55 @@ using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
-  public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
-        var pattern = @"\w+[a-z0-9A-Z]";
-        var myRegex = new Regex(pattern);
-        
-        var input = new List<String>(lines);
-        var ans = new List<String>();
-        string combinedString = string.Join( " ", input);
-        
-        foreach(Match m in myRegex.Matches(combinedString)){
-          ans.Add(m.ToString());
-        }
-
-        return ans;
-  }
-
-  public static IEnumerable<(int width, int height)> Resolution(string resolutions){
-    var pattern = @"(?<width>\d+)x(?<height>\d+)";
+  public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+  {
+    var pattern = @"\w+[a-z0-9A-Z]";
     var myRegex = new Regex(pattern);
-        
-    var output = new List<(int,int)>();
-    
-    foreach(Match m in myRegex.Matches(resolutions)){
-      var width = m.Groups["width"].Value;
-      var height = m.Groups["height"].Value;
-      output.Add((Int32.Parse(width),Int32.Parse(height)));
+
+    var input = new List<String>(lines);
+    var ans = new List<String>();
+    string combinedString = string.Join(" ", input);
+
+    foreach (Match m in myRegex.Matches(combinedString))
+    {
+      ans.Add(m.ToString());
     }
 
-    return output;    
+    return ans;
   }
 
-  public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+  public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+  {
+    var pattern = @"(?<width>\d+)x(?<height>\d+)";
+    var myRegex = new Regex(pattern);
+
+    var output = new List<(int, int)>();
+
+    foreach (Match m in myRegex.Matches(resolutions))
+    {
+      var width = m.Groups["width"].Value;
+      var height = m.Groups["height"].Value;
+      output.Add((Int32.Parse(width), Int32.Parse(height)));
+    }
+
+    return output;
+  }
+
+  public static IEnumerable<string> InnerText(string html, string tag)
+  {
+    var pattern = $@"<{tag}.*?>(?<text>.*?)</{tag}>";
+    var myRegex = new Regex(pattern);
+
+    var output = new List<String>();
+
+    foreach (Match m in myRegex.Matches(html))
+    {
+      var text = m.Groups["text"].Value;
+      output.Add(text);
+    }
+
+    return output;
+  }
 
   public static IEnumerable<(Uri url, string title)> Urls(string html)
   {
